1824be965764dcdc8243257717085dacfc7253ea
Ace-Boogie <desbonnes.marvin@gmail.com>
2026-06-17 14:33:53 +0200

    fix error build

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// Configuration TypeScript pour ignorer l'erreur de type à supprimer quand App sera en TypeScript
-// @ts-expect-error
 import App from "./components/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
